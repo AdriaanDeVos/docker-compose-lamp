16be05c47793d319ca9ca926f63d4a9f6e581b2a
Adriaan de Vos <adriaan.devos@gmail.com>
2022-07-11 13:10:11 +0200

    Add basic styling and empty grid

--- www/index.php
+++ www/index.php
@@ -4,9 +4,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>NLO - Grid Challenge</title>
-        <link rel="script" href="./js/index.js">
+        <script src="./js/index.js"></script>
+        <style>
+            tr {
+                height: 15px;
+            }
+            td {
+                width: 15px;
+                border: 1px solid black;
+            }
+            .opened {
+                background-color: red;
+            }
+        </style>
     </head>
     <body>
         <h1>NLO - Grid Challenge</h1>
+        <table id="grid"></table>
     </body>
 </html>
